cf2ae7159b84c97a6d3ceb6b49313090dcd28eb9
planetabhi <abhimanyu.rana@postman.com>
2024-01-15 16:10:57 +0530

    cleanup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 export default {
     lang: 'en-US',
     title: 'Sacred Books of the East',
-    colorMode: 'light',
+    colorMode: 'auto',
     description: 'The books presented here are either original scans of books in the public domain, material that has been presented elsewhere on the internet, or material included under fair use conditions in printed anthologies.',
     theme: defaultTheme({
         logo: './images/logo-light.png',
